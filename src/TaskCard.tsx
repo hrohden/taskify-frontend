5bda9346daae9a0ccadef01dd5d2294e85bc6606
Henrique Broering Rohden <hrohden@gmail.com>
2023-12-07 17:03:21 -0300

    Creates status badge indicator

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -1,17 +1,22 @@
 import { Task } from "./Task";
 
-type Props = {
-  task: Task;
+const TaskStatus = ({ status }: { status: string }) => {
+  return (
+    <span className="rounded-full bg-green-200 px-3 py-1 text-xs font-semibold text-green-700">
+      {status}
+    </span>
+  );
 };
-const TaskCard = ({ task }: Props) => {
+
+const TaskCard = ({ task }: { task: Task }) => {
   return (
     <div className="flex flex-col rounded-md border border-gray-300 shadow">
       <div className="flex justify-center rounded-t-md p-2 font-semibold">
         {task.title}
       </div>
       <div className="p-2 text-sm">{task.description}</div>
-      <div className="flex justify-end rounded-b-md bg-gray-100 p-2">
-        {task.status}
+      <div className="flex justify-end rounded-b-md border-t border-t-gray-200 bg-gray-50 p-2">
+        <TaskStatus status={task.status} />
       </div>
     </div>
   );
