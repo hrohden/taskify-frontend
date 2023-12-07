8b6ed8db9c671beffc42d8d1a92aad5253fb540f
Henrique Broering Rohden <hrohden@gmail.com>
2023-12-07 18:40:25 -0300

    Adds styiling to the columns

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -10,12 +10,12 @@ const TaskStatus = ({ status }: { status: string }) => {
 
 const TaskCard = ({ task }: { task: Task }) => {
   return (
-    <div className="flex flex-col rounded-md border border-gray-300 shadow">
-      <div className="flex justify-center rounded-t-md p-2 font-semibold">
+    <div className="flex w-full flex-col rounded-md border border-gray-300 shadow">
+      <div className="flex w-full justify-center rounded-t-md p-2 font-semibold">
         {task.title}
       </div>
-      <div className="p-2 text-sm">{task.description}</div>
-      <div className="flex justify-end rounded-b-md border-t border-t-gray-200 bg-gray-50 p-2">
+      <div className="w-full p-2 text-sm">{task.description}</div>
+      <div className="flex w-full justify-end rounded-b-md border-t border-t-gray-200 bg-gray-50 p-2">
         <TaskStatus status={task.status} />
       </div>
     </div>
