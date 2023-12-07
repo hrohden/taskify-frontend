17a0df8813adcb745b2d7113e5a1edfb9f9ad00c
Henrique Broering Rohden <hrohden@gmail.com>
2023-12-07 16:18:54 -0300

    Create initial task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { Task } from "./Task";
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      status: "PENDING",
+      title: "Create Taskify application",
+      description: "Initial code to showcase some features.",
+    },
+  ]);
+
   return (
     <div className="flex justify-center">
       <h1 className="font-bold text-4xl">Taskify</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
