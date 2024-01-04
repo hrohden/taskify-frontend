a033c9c0604fca0dfabe9793bc63e54fe23dcd99
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-04 18:21:31 -0300

    Shows previous and next buttons conditionally

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -6,36 +6,46 @@ import { Task } from "./types/Task";
 
 const TaskCard = ({ task }: { task: Task }) => {
   const dispatch = useDispatch();
+  const minStatusId = statusList[0].id;
+  const maxStatusId = statusList[statusList.length - 1].id;
   return (
     <div className="flex w-full flex-col rounded-md border border-gray-300 shadow">
       <div className="flex w-full justify-stretch gap-1 rounded-t-md p-2 font-semibold">
         <div className="w-full text-center">{task.title}</div>
-        <button
-          className="w-8 rounded-md border border-gray-200 p-1 text-sm text-gray-400"
-          onClick={() => {
-            dispatch(
-              move({
-                id: task.id,
-                status: statusList.find((s) => s.id === task.status.id - 1)!,
-              }),
-            );
-          }}
-        >
-          ◀
-        </button>
-        <button
-          className="w-8 rounded-md border border-gray-200 p-1 text-sm text-gray-400"
-          onClick={() => {
-            dispatch(
-              move({
-                id: task.id,
-                status: statusList.find((s) => s.id === task.status.id + 1)!,
-              }),
-            );
-          }}
-        >
-          ▶
-        </button>
+        {task.status.id > minStatusId && (
+          <button
+            className="w-8 rounded-md border border-gray-200 p-1 text-sm text-gray-400"
+            onClick={() => {
+              dispatch(
+                move({
+                  id: task.id,
+                  status: statusList.find((s) => s.id === task.status.id - 1)!,
+                }),
+              );
+            }}
+          >
+            ◀
+          </button>
+        )}
+        {task.status.id < maxStatusId && (
+          <button
+            className="w-8 rounded-md border border-gray-200 p-1 text-sm text-gray-400"
+            onClick={() => {
+              const newStatusId =
+                task.status.id + 1 > maxStatusId
+                  ? maxStatusId
+                  : task.status.id + 1;
+              dispatch(
+                move({
+                  id: task.id,
+                  status: statusList.find((s) => s.id === newStatusId)!,
+                }),
+              );
+            }}
+          >
+            ▶
+          </button>
+        )}
       </div>
       <div className="w-full p-2 text-sm">{task.description}</div>
       <div className="flex w-full justify-end rounded-b-md border-t border-t-gray-200 bg-gray-50 p-2">
