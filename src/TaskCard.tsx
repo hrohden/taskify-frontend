6bbc5e74257948f6243027c1844d0cce415a8e33
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-05 16:22:04 -0300

    Refactor TaskCard and TaskStatus components

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -19,7 +19,7 @@ const TaskCard = ({ task }: { task: Task }) => {
           <div className="inline-flex rounded-full border border-gray-200 p-0.5 dark:border-gray-700">
             {task.status.id > minStatusId && (
               <button
-                className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-1 dark:focus:ring-gray-600 dark:focus:ring-gray-600"
+                className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 onClick={() => {
                   dispatch(
                     move({
@@ -36,7 +36,7 @@ const TaskCard = ({ task }: { task: Task }) => {
             )}
             {task.status.id < maxStatusId && (
               <button
-                className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-1 dark:focus:ring-gray-600 dark:focus:ring-gray-600"
+                className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 onClick={() => {
                   const newStatusId =
                     task.status.id + 1 > maxStatusId
@@ -59,7 +59,7 @@ const TaskCard = ({ task }: { task: Task }) => {
           {task.description}
         </p>
       </div>
-      <div className="rounded-b-xl border-t bg-gray-100 px-4 py-3 dark:border-gray-700 dark:bg-slate-900 md:px-5 md:py-4">
+      <div className="flex justify-end rounded-b-xl border-t bg-gray-100 px-4 py-3 dark:border-gray-700 dark:bg-slate-900 md:px-5 md:py-4">
         <TaskStatus status={task.status} />
       </div>
     </div>
--- src/TaskStatus.tsx
+++ src/TaskStatus.tsx
@@ -4,29 +4,27 @@ const TaskStatus = ({ status }: { status: Status }) => {
   const styles = [
     {
       id: 1,
-      styles: { text: "text-slate-700", background: "bg-slate-200" },
+      className:
+        "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-white/10 dark:text-white",
     },
     {
       id: 2,
-      styles: { text: "text-green-700", background: "bg-green-200" },
+      className:
+        "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-teal-100 text-teal-800 dark:bg-teal-800/30 dark:text-teal-500",
     },
     {
       id: 3,
-      styles: { text: "text-red-700", background: "bg-red-200" },
+      className:
+        "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500",
     },
     {
       id: 4,
-      styles: { text: "text-purple-700", background: "bg-purple-200" },
+      className:
+        "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500",
     },
   ];
-  const selectedStyle = styles.find((s) => s.id === status.id)!.styles;
-  return (
-    <span
-      className={`rounded-full ${selectedStyle.background} px-3 py-1 text-xs font-semibold ${selectedStyle.text}`}
-    >
-      {status.description}
-    </span>
-  );
+  const selectedStyle = styles.find((s) => s.id === status.id)!.className;
+  return <span className={selectedStyle}>{status.description}</span>;
 };
 
 export default TaskStatus;
