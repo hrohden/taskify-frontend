24ae5bd7fb1b780afd72b5e51d2cbdf9f0c3dd04
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-08 19:52:20 -0300

    Refactor Board component to use useGetAllTasksQuery hook

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,19 +1,27 @@
-import { useSelector } from "react-redux";
 import BoardColumn from "./BoardColumn";
+import { useGetAllTasksQuery } from "./app/taskSlice";
 import { statusList } from "./types/Status";
-import { Store } from "./types/Store";
 
 const Board = () => {
-  const tasks = useSelector((state: Store) => state.tasks);
+  // const tasks = useSelector((state: Store) => state.tasks);
+  const { data, error, isLoading } = useGetAllTasksQuery();
   return (
     <div className="flex w-full justify-between gap-3">
-      {statusList.map((status) => (
-        <BoardColumn
-          key={status.id}
-          title={status.description}
-          tasks={tasks.filter((task) => task.status.id === status.id)}
-        />
-      ))}
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <div>
+          {statusList.map((status) => (
+            <BoardColumn
+              key={status.id}
+              title={status.description}
+              tasks={data.filter((task) => task.status.id === status.id)}
+            />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
