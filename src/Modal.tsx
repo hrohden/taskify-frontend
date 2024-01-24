1247ab965581b53a81bef4733eb2fe556b60c8e1
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-24 15:43:20 -0300

    Refactor modal styling

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -11,10 +11,10 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
 
   return (
     <div
-      className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-50"
+      className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-50 backdrop-blur"
       onClick={onClose}
     >
-      <div className="relative left-2/4 top-2/4 w-1/3 -translate-x-2/4 -translate-y-2/4 rounded-lg bg-white p-8">
+      <div className="relative left-2/4 top-2/4 w-1/3 -translate-x-2/4 -translate-y-2/4 rounded-xl bg-white p-8 shadow-lg">
         {children}
         <button onClick={onClose}>Close</button>
       </div>
