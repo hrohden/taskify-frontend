936b06715d07aebc8d4c9d3ed7b0d590524c21de
Henrique Broering Rohden <hrohden@gmail.com>
2024-01-24 16:44:28 -0300

    Refactor NewTaskModalForm component

--- src/NewTaskModalForm.tsx
+++ src/NewTaskModalForm.tsx
@@ -27,10 +27,37 @@ const NewTaskModalForm = (props: NewTaskModalFormProps) => {
           console.log(data);
           // createTask({ id: task.id, ...data });
         })}
+        className="grid gap-y-4"
       >
-        <input type="text" {...form.register("title")} />
-        <input type="text" {...form.register("description")} />
-        <button type="submit">Submit</button>
+        <div>
+          <label htmlFor="title" className="mb-2 block text-sm dark:text-white">
+            Title (mandatory)
+          </label>
+          <input
+            type="text"
+            {...form.register("title")}
+            className="block w-full rounded-lg border border-gray-200 px-4 py-3 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="description"
+            className="mb-2 block text-sm dark:text-white"
+          >
+            Description
+          </label>
+          <input
+            type="text"
+            {...form.register("description")}
+            className="block w-full rounded-lg border border-gray-200 px-4 py-3 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
+          />
+        </div>
+        <button
+          type="submit"
+          className="inline-flex w-full items-center justify-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-4 py-3 text-sm font-semibold text-white hover:bg-blue-700 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        >
+          Submit
+        </button>
       </form>
     </Modal>
   );
